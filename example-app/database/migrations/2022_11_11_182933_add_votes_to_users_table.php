ae6dacb292ddd32366dbe7e9b48b56de1f08d43e
mac-book <lexx.mg@gmail.com>
2022-11-11 21:58:16 +0300

    add migration users phone_id

---
+++ example-app/database/migrations/2022_11_11_182933_add_votes_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddVotesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('email', function ($table) {
+                $table->foreignId('phone_id')->unique()->nullable()->constrained()->nullOnDelete();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // $table->dropUnique('phone_id');
+            $table->dropForeign(['phone_id']);
+        });
+    }
+}
