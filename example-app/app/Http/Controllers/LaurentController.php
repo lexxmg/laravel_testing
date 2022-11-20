74add395bb23f42dc49495d9ddda6f1562549f4d
mac-book <lexx.mg@gmail.com>
2022-11-20 12:35:11 +0300

    edit laurent

--- example-app/app/Http/Controllers/LaurentController.php
+++ example-app/app/Http/Controllers/LaurentController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Laurent\Laurent;
+use App\Models\Out;
 use Illuminate\Http\Request;
 
 class LaurentController extends Controller
 {
     public function laurent()
     {
+        $out = Out::find(1);
+        $host = $out->laurent_id;
+        //dd($out->laurent_id);
         //$test = Laurent::test();
-        $test = Laurent::getStatusOut('http://192.168.0.101', 3);
+        $test = Laurent::getStatusOut('http://' . $host, 3);
 
         return view('laurent.laurent', ['test' => $test]);
     }
